464f93bf67f1e40d3c1f3ca90a447ee724e9658b
Bridgey the Geek <bridgeythegeek@users.noreply.github.com>
2016-05-05 11:33:23 +0100

    Added new editbox.py
    Start of overhaul of editbox.

---
+++ editbox.py
@@ -0,0 +1,193 @@
+# Volatility EditBox plugin
+#
+# Author: Bridgey the Geek <bridgeythegeek@gmail.com>
+#
+# This plugin is free software; you can redistribute it and/or modify
+# it under the terms of GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This plugin is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PRACTICAL PURPOSE. See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this plugin. If not, see <http://www.gnu.org/licenses/>.
+#
+# This work heavily inspired by  GDI Utilities from Dr Brendan Dolan-Gavitt PhD.
+# <http://www.cc.gatech.edu/~brendan/volatility/>
+#
+# The iteration of the Windows objects is borrowed from the Windows plugin.
+# <https://github.com/volatilityfoundation/volatility/blob/master/volatility/plugins/gui/windows.py>
+#
+# This plugin wouldn't exist without the assistance of those on the volusers
+# mailing list <http://lists.volatilesystems.com/mailman/listinfo/vol-users>.
+
+"""
+@author     : Bridgey the Geek
+@license    : GPL 2 or later
+@contact    : bridgeythegeek@gmail.com
+"""
+
+import volatility.debug as debug
+import volatility.obj as obj
+import volatility.utils as utils
+
+import volatility.plugins.common as common
+import volatility.plugins.gui.messagehooks as messagehooks
+import volatility.win32 as win32
+
+
+editbox_vtypes_xp_x86 = {
+}
+
+editbox_vtypes_xp_x64 = {
+}
+
+editbox_vtypes_vista7_x86 = {
+}
+
+editbox_vtypes_vista7_x64 = {
+    'COMCTL_EDIT': [ 0x142, {
+        'hBuf': [0x00, ['pointer', ['pointer', ['unsigned long']]]],
+        'hWnd': [0x40, ['unsigned long']],
+        'parenthWnd': [0x60, ['unsigned long']],
+        'nChars': [0x10, ['unsigned long']],
+        'selStart': [0x18, ['unsigned long']],
+        'selEnd': [0x20, ['unsigned long']],
+        'pwdChar': [0x34, ['unsigned short']],
+        'undoBuf': [0xA8, ['unsigned long']],
+        'undoPos': [0xB0, ['long']],
+        'undoLen': [0xB4, ['long']],
+        'bEncKey': [0x140, ['unsigned char']],
+    }]
+}
+
+class COMCTL_EDIT(obj.CType):
+    """Methods for the Edit structure"""
+    
+    def __str__(self):
+        """String representation of the Edit"""
+        
+        _MAX_OUT = 50
+        
+        text = self.get_text(self.hBuf, self.nChars)
+        text_len = len(text)
+        text = '{}...'.format(
+            text[:_MAX_OUT - 3]) if len(text) > _MAX_OUT else text
+        
+        undo = self.get_text(self.undoBuf, self.undoLen)
+        undo_len = len(undo)
+        undo = '{}...'.format(
+            undo[:_MAX_OUT - 3]) if len(undo) > _MAX_OUT else undo
+        
+        return 'COMCTL_EDIT <Text={0}, Len={1}, Undo={2}, UndoLen={3}>'.format(
+            text, text_len, undo, undo_len)
+    
+    def get_text(self, offset, len):
+        # Check pwdChar
+        string = self.obj_vm.read(offset, len * 2).decode('utf-16')
+        return '' if not string else string
+
+
+class Editbox2(common.AbstractWindowsCommand):
+    """Displays information about Edit controls. (Listbox experimental.)"""
+    
+    # Add the classes for the structures
+    editbox_classes = {
+        'COMCTL_EDIT': COMCTL_EDIT,
+    }
+    
+    # Map the version of Windows to the correct vtypes
+    version_map = {
+        'windows': {
+            5: {
+                '32bit': editbox_vtypes_xp_x86,
+                '64bit': editbox_vtypes_xp_x64,
+            },
+            6: {
+                '32bit': editbox_vtypes_vista7_x86,
+                '64bit': editbox_vtypes_vista7_x64,
+            },
+        }
+    }
+    
+    def __init__(self, config, *args, **kwargs):
+        common.AbstractWindowsCommand.__init__(self, config, *args, **kwargs)
+        
+        # Filter specific processes
+        config.add_option('PID', short_option = 'p', default = None,
+            help = 'Operate on these Process IDs (comma-separated)',
+            action = 'store', type = 'str')
+    
+    @staticmethod
+    def apply_types(addr_space):
+        """Add the correct vtypes and classes for the profile
+        
+        @param  addr_space: <volatility.BaseAddressSpace>
+        """
+        
+        meta = addr_space.profile.metadata
+        try:
+            vtypes = Editbox2.version_map[
+                meta['os']][meta['major']][meta['memory_model']]
+            addr_space.profile.vtypes.update(vtypes)
+            addr_space.profile.object_classes.update(Editbox2.editbox_classes)
+            addr_space.profile.compile()
+            
+        except KeyError:
+            debug.error("The selected address space is not supported")
+    
+    def calculate(self):
+        """Parse the control structure"""
+        
+        # Apply the correct vtypes for the profile
+        addr_space = utils.load_as(self._config)
+        self.apply_types(addr_space)
+        
+        # Build a list of tasks
+        tasks = win32.tasks.pslist(addr_space)
+        if self._config.PID:
+            pids = [int(p) for p in self._config.PID.split(',')]
+            the_tasks = [t for t in tasks if t.UniqueProcessId in pids]
+        else:
+            the_tasks = [t for t in tasks]
+        
+        # In case no PIDs found
+        if len(the_tasks) < 1:
+            return
+        
+        # Iterate through all the window objects matching for !Edit
+        mh = messagehooks.MessageHooks(self._config)
+        for winsta, atom_tables in mh.calculate():
+            for desktop in winsta.desktops():
+                for wnd, _level in desktop.windows(desktop.DeskInfo.spwnd):
+                    if wnd.Process in the_tasks:
+                        
+                        atom_class = mh.translate_atom(winsta,
+                            atom_tables, wnd.ClassAtom)
+                        
+                        if atom_class.endswith("!Edit"):
+                            task_vm = wnd.Process.get_process_address_space()
+                            
+                            wndextra_offset = wnd.v() + addr_space.profile.get_obj_size('tagWND')
+                            wndextra = obj.Object('address', offset=wndextra_offset, vm=task_vm)
+                            edit = obj.Object('COMCTL_EDIT', offset=wndextra, vm=task_vm)
+                            yield wnd.Process.UniqueProcessId, wnd.Process.ImageFileName, edit
+    
+    def render_text(self, outfd, data):
+        """Output the results as text
+        
+        @param  outfd: <file>
+        @param  data: <generator>
+        """
+        
+        self.table_header(outfd, [
+            ('PID', '6'),
+            ('Process', '14'),
+            ('Control', ""),
+            ])
+        
+        for pid, proc_name, edit in data:
+            self.table_row(outfd, pid, proc_name, str(edit))
