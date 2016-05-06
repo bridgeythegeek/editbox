535c7f9174b2dc51ba24ce783e92ecd7c4c6956a
Bridgey the Geek <bridgeythegeek@gmail.com>
2016-05-06 21:48:10 +0100

    Added output as text and dump to file.
    Output options are now text and table. Added --dump-dir/-D for saving ctrl text to file.

--- editbox.py
+++ editbox.py
@@ -30,6 +30,8 @@
 @contact    : bridgeythegeek@gmail.com
 """
 
+import os
+
 import volatility.debug as debug
 import volatility.obj as obj
 import volatility.utils as utils
@@ -38,19 +40,18 @@ import volatility.plugins.common as common
 import volatility.plugins.gui.messagehooks as messagehooks
 import volatility.win32 as win32
 
-
 supported_controls = {
     'edit': 'COMCTL_EDIT',
 }
 
-#---------------------------------------------------------------------
+# ---------------------------------------------------------------------
 # Edit
-#---------------------------------------------------------------------
+# ---------------------------------------------------------------------
 editbox_vtypes_xp_x86 = {
-    'COMCTL_EDIT': [ 0xEE, {
-        'hBuf': [ 0x00, ['pointer', ['pointer', ['unsigned long']]]],
-        'hWnd': [ 0x38, ['unsigned long']],
-        'parenthWnd': [ 0x58, ['unsigned long']],
+    'COMCTL_EDIT': [0xEE, {
+        'hBuf': [0x00, ['pointer', ['pointer', ['unsigned long']]]],
+        'hWnd': [0x38, ['unsigned long']],
+        'parenthWnd': [0x58, ['unsigned long']],
         'nChars': [0x0C, ['unsigned long']],
         'selStart': [0x14, ['unsigned long']],
         'selEnd': [0x18, ['unsigned long']],
@@ -66,10 +67,10 @@ editbox_vtypes_xp_x64 = {
 }
 
 editbox_vtypes_vista7_x86 = {
-    'COMCTL_EDIT': [ 0xF6, {
-        'hBuf': [ 0x00, ['pointer', ['pointer', ['unsigned long']]]],
-        'hWnd': [ 0x38, ['unsigned long']],
-        'parenthWnd': [ 0x58, ['unsigned long']],
+    'COMCTL_EDIT': [0xF6, {
+        'hBuf': [0x00, ['pointer', ['pointer', ['unsigned long']]]],
+        'hWnd': [0x38, ['unsigned long']],
+        'parenthWnd': [0x58, ['unsigned long']],
         'nChars': [0x0C, ['unsigned long']],
         'selStart': [0x14, ['unsigned long']],
         'selEnd': [0x18, ['unsigned long']],
@@ -82,7 +83,7 @@ editbox_vtypes_vista7_x86 = {
 }
 
 editbox_vtypes_vista7_x64 = {
-    'COMCTL_EDIT': [ 0x142, {
+    'COMCTL_EDIT': [0x142, {
         'hBuf': [0x00, ['pointer', ['pointer', ['unsigned long']]]],
         'hWnd': [0x40, ['unsigned long']],
         'parenthWnd': [0x60, ['unsigned long']],
@@ -100,53 +101,88 @@ editbox_vtypes_vista7_x64 = {
 
 class COMCTL_EDIT(obj.CType):
     """Methods for the Edit structure"""
-    
+
     def __str__(self):
         """String representation of the Edit"""
-        
+
         _MAX_OUT = 50
-        
+
         text = self.get_text()
         text = '{}...'.format(text[:_MAX_OUT - 3]) if len(text) > _MAX_OUT else text
-        
+
         undo = self.get_undo()
         undo = '{}...'.format(undo[:_MAX_OUT - 3]) if len(undo) > _MAX_OUT else undo
-        
-        return '<COMCTL_EDIT(Text={0}, Len={1}, Pwd={2}, Undo={3}, UndoLen={4})>'.format(
-            text, self.nChars, self.is_pwd(), undo, self.undoLen)
-    
+
+        return \
+            '<COMCTL_EDIT(Text={0}, Len={1}, Pwd={2}, Undo={3}, UndoLen={4})>'.format(
+                text, self.nChars, self.is_pwd(), undo, self.undoLen)
+
     def get_text(self):
         if self.nChars < 1:
             return ''
         text_deref = obj.Object('address', offset=self.hBuf, vm=self.obj_vm)
-        bytes = self.obj_vm.read(text_deref, self.nChars * 2)
+        raw = self.obj_vm.read(text_deref, self.nChars * 2)
         if not self.pwdChar == 0x00:  # Is a password dialog
-            bytes = COMCTL_EDIT.RtlRunDecodeUnicodeString(self.bEncKey, bytes)
-        return bytes.decode('utf-16')
-    
+            raw = COMCTL_EDIT.rtl_run_decode_unicode_string(self.bEncKey, raw)
+        return raw.decode('utf-16')
+
     def get_undo(self):
         if self.undoLen < 1:
             return ''
         return self.obj_vm.read(self.undoBuf, self.undoLen * 2).decode('utf-16')
-    
+
     def is_pwd(self):
         return self.pwdChar != 0x00
-    
+
+    def dump_meta(self, outfd):
+        """Dumps the meta data of the control
+        
+        @param  outfd: 
+        """
+        outfd.write('nChars            : {}\n'.format(self.nChars))
+        outfd.write('selStart          : {}\n'.format(self.selStart))
+        outfd.write('selEnd            : {}\n'.format(self.selEnd))
+        outfd.write('isPwdControl      : {}\n'.format(self.is_pwd()))
+        outfd.write('undoPos           : {}\n'.format(self.undoPos))
+        outfd.write('undoLen           : {}\n'.format(self.undoLen))
+        outfd.write('address-of undoBuf: {:#x}\n'.format(self.undoBuf))
+        outfd.write('undoBuf           : {}\n'.format(self.get_undo()))
+
+    def dump_data(self, outfd):
+        """Dumps the data of the control
+        
+        @param  outfd: 
+        """
+        outfd.write('{}\n'.format(self.get_text()))
+
     @staticmethod
-    def RtlRunDecodeUnicodeString(key, data):
-        s = ''.join([chr(ord(data[i-1]) ^ ord(data[i]) ^ key) for i in range(1,len(data))])
+    def rtl_run_decode_unicode_string(key, data):
+        s = ''.join([chr(ord(data[i - 1]) ^ ord(data[i]) ^ key) for i in range(1, len(data))])
         s = chr(ord(data[0]) ^ (key | 0x43)) + s
         return s
 
 
+def dump_to_file(ctrl, pid, proc_name, folder):
+    """Dumps the data of the control to a file
+
+    @param  ctrl:
+    @param  pid:
+    @param  proc_name:
+    """
+    ctrl_safe_name = str(ctrl.__class__.__name__).split('_')[-1].lower()
+    file_name = '{0}_{1}_{2}_{3:#x}.txt'.format(pid, proc_name, ctrl_safe_name, ctrl.v())
+    with open(os.path.join(folder, file_name), 'wb') as out_file:
+        out_file.write(ctrl.get_text())
+
+
 class Editbox2(common.AbstractWindowsCommand):
     """Displays information about Edit controls. (Listbox experimental.)"""
-    
+
     # Add the classes for the structures
     editbox_classes = {
         'COMCTL_EDIT': COMCTL_EDIT,
     }
-    
+
     # Map the version of Windows to the correct vtypes
     version_map = {
         'windows': {
@@ -160,22 +196,25 @@ class Editbox2(common.AbstractWindowsCommand):
             },
         }
     }
-    
+
     def __init__(self, config, *args, **kwargs):
         common.AbstractWindowsCommand.__init__(self, config, *args, **kwargs)
-        
+
         # Filter specific processes
-        config.add_option('PID', short_option = 'p', default = None,
-            help = 'Operate on these Process IDs (comma-separated)',
-            action = 'store', type = 'str')
-    
+        config.add_option('PID', short_option='p', default=None,
+                          help='Operate on these Process IDs (comma-separated)',
+                          action='store', type='str')
+        config.add_option('DUMP-DIR', short_option='D', default=None,
+                          help='Save the found text to files in this folder',
+                          action='store', type='str')
+
     @staticmethod
     def apply_types(addr_space):
         """Add the correct vtypes and classes for the profile
         
         @param  addr_space: <volatility.BaseAddressSpace>
         """
-        
+
         meta = addr_space.profile.metadata
         try:
             vtypes = Editbox2.version_map[
@@ -183,17 +222,21 @@ class Editbox2(common.AbstractWindowsCommand):
             addr_space.profile.vtypes.update(vtypes)
             addr_space.profile.object_classes.update(Editbox2.editbox_classes)
             addr_space.profile.compile()
-            
+
         except KeyError:
             debug.error("The selected address space is not supported")
-    
+
     def calculate(self):
-        """Parse the control structure"""
-        
+        """Parse the control structures"""
+
+        # Check the output folder exists
+        if self._config.DUMP_DIR and not os.path.isdir(self._config.dump_dir):
+            debug.error('{0} is not a directory'.format(self._config.dump_dir))
+
         # Apply the correct vtypes for the profile
         addr_space = utils.load_as(self._config)
         self.apply_types(addr_space)
-        
+
         # Build a list of tasks
         tasks = win32.tasks.pslist(addr_space)
         if self._config.PID:
@@ -201,44 +244,66 @@ class Editbox2(common.AbstractWindowsCommand):
             the_tasks = [t for t in tasks if t.UniqueProcessId in pids]
         else:
             the_tasks = [t for t in tasks]
-        
+
         # In case no PIDs found
         if len(the_tasks) < 1:
             return
-        
+
         # Iterate through all the window objects matching for supported controls
         mh = messagehooks.MessageHooks(self._config)
         for winsta, atom_tables in mh.calculate():
             for desktop in winsta.desktops():
                 for wnd, _level in desktop.windows(desktop.DeskInfo.spwnd):
                     if wnd.Process in the_tasks:
-                        
-                        atom_class = mh.translate_atom(winsta,
-                            atom_tables, wnd.ClassAtom)
-                        
+
+                        atom_class = mh.translate_atom(winsta, atom_tables, wnd.ClassAtom)
                         if atom_class:
                             atom_class = str(atom_class)
                             if '!' in atom_class:
                                 comctl_class = atom_class.split('!')[-1].lower()
                                 if comctl_class in supported_controls:
-                                    task_vm = wnd.Process.get_process_address_space()                                
+                                    context = '{0}\\{1}\\{2}'.format(winsta.dwSessionId, winsta.Name, desktop.Name)
+                                    task_vm = wnd.Process.get_process_address_space()
                                     wndextra_offset = wnd.v() + addr_space.profile.get_obj_size('tagWND')
                                     wndextra = obj.Object('address', offset=wndextra_offset, vm=task_vm)
                                     ctrl = obj.Object(supported_controls[comctl_class], offset=wndextra, vm=task_vm)
-                                    yield wnd.Process.UniqueProcessId, wnd.Process.ImageFileName, ctrl
-    
-    def render_text(self, outfd, data):
-        """Output the results as text
+                                    if self._config.DUMP_DIR:
+                                        dump_to_file(ctrl, wnd.Process.UniqueProcessId,
+                                                     wnd.Process.ImageFileName, self._config.DUMP_DIR)
+                                    yield context, atom_class, wnd.Process.UniqueProcessId, \
+                                        wnd.Process.ImageFileName, ctrl
+
+    def render_table(self, outfd, data):
+        """Output the results as a table
         
         @param  outfd: <file>
         @param  data: <generator>
         """
-        
+
         self.table_header(outfd, [
             ('PID', '6'),
             ('Process', '14'),
             ('Control', ""),
-            ])
+        ])
+
+        for context, atom_class, pid, proc_name, ctrl in data:
+            # context and atom_class are ignored
+            self.table_row(outfd, pid, proc_name, str(ctrl))
+
+    def render_text(self, outfd, data):
+        """Output the results as a text report
         
-        for pid, proc_name, edit in data:
-            self.table_row(outfd, pid, proc_name, str(edit))
+        @param  outfd: <file>
+        @param  data: <generator>
+        """
+
+        for context, atom_class, pid, proc_name, ctrl in data:
+            outfd.write('{}\n'.format('*' * 30))
+            outfd.write('Wnd Context       : {}\n'.format(context))
+            outfd.write('Process ID        : {}\n'.format(pid))
+            outfd.write('ImageFileName     : {}\n'.format(proc_name))
+            outfd.write('atom_class        : {}\n'.format(atom_class))
+            outfd.write('value-of WndExtra : {:#x}\n'.format(ctrl.v()))
+            ctrl.dump_meta(outfd)
+            outfd.write('{}\n'.format('-' * 25))
+            ctrl.dump_data(outfd)
